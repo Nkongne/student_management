0e3c7b68eca519a358aff3d2ffb48ea7320c7997
nkongne <nkongne@gmail.com>
2021-03-09 18:11:03 +0100

    creating ignores file

--- student_management/settings.py
+++ student_management/settings.py
@@ -135,8 +135,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, '../registration/static')]
+STATIC_URL = 'root/app/static/'
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, '../registration/static')]
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 LANGUAGES=(
     ('en', _('English')),
